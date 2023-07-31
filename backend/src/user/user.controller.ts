f66ddb7ca4bde26136a7108241809bd24a6989ef
achatela <achatela@student.42.fr>
2023-07-31 17:34:59 +0200

    friends done in backend

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -19,4 +19,34 @@ export class UserController {
     async getUserInfos(data: { username: string }) {
         return await this.userService.getUserInfos(data.username);
     }
+
+    @Post('add_friend')
+    async addFriend(data: { username: string, friendUsername: string }) {
+        return await this.userService.addFriend(data.username, data.friendUsername);
+    }
+
+    @Post('accept_friend')
+    async acceptFriend(data: { username: string, toAcceptUsername: string }) {
+        return await this.userService.acceptFriend(data.username, data.toAcceptUsername);
+    }
+
+    @Post('decline_friend')
+    async declineFriend(data: { username: string, toDeclineUsername: string }) {
+        return await this.userService.declineFriend(data.username, data.toDeclineUsername);
+    }
+
+    @Post('delete_friend')
+    async deleteFriend(data: { username: string, toDeleteUsername: string }) {
+        return await this.userService.deleteFriend(data.username, data.toDeleteUsername);
+    }
+
+    @Post('get_friend_requests')
+    async getFriendRequests(data: { username: string }) {
+        return await this.userService.getFriendRequests(data.username);
+    }
+
+    @Post('get_friends')
+    async getFriends(data: { username: string }) {
+        return await this.userService.getFriends(data.username);
+    }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,10 +3,152 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    getUserInfos(username: string) {
-        throw new Error('Method not implemented.');
-    }
     constructor(private prismaService: PrismaService) { }
+
+    // Friends related functions
+
+    async getFriends(username: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        if (user.friends.length == 0)
+            return { success: false, error: 'No friends' }
+        let friends = [];
+        for (let i = 0; i < user.friends.length; i++) {
+            const friend = await this.prismaService.user.findUnique({ where: { id: user.friends[i] } })
+            friends.push({
+                username: friend.name,
+                image: friend.image,
+                balance: friend.balance,
+            })
+        }
+        return { success: true, friends: friends }
+    }
+
+    async getFriendRequests(username: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        if (user.friendsRequests.length == 0)
+            return { success: false, error: 'No friend requests' }
+        let friendRequests = [];
+        for (let i = 0; i < user.friendsRequests.length; i++) {
+            const friend = await this.prismaService.user.findUnique({ where: { id: user.friendsRequests[i] } })
+            friendRequests.push({
+                username: friend.name,
+                image: friend.image,
+                balance: friend.balance,
+            })
+        }
+        return { success: true, friendRequests: friendRequests }
+    }
+
+    async addFriend(username: string, friendUsername: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        const friend = await this.prismaService.user.findUnique({ where: { name: friendUsername } })
+        if (friend == null)
+            return { success: false, error: 'Friend not found' }
+        if (user.friends.includes(friend.id) || friend.friends.includes(user.id))
+            return { success: false, error: 'Already friends' }
+        else if (user.friendsRequests.includes(friend.id) || friend.friendsRequests.includes(user.id))
+            return { success: false, error: 'Already sent a friend request' }
+        else {
+            await this.prismaService.user.update({
+                where: { name: friendUsername },
+                data: { friendsRequests: { push: user.id } }
+            })
+            return { success: true }
+        }
+    }
+
+    async acceptFriend(username: string, toAcceptUsername: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        const toAccept = await this.prismaService.user.findUnique({ where: { name: toAcceptUsername } })
+        if (toAccept == null)
+            return { success: false, error: 'Friend not found' }
+        if (user.friends.includes(toAccept.id) || toAccept.friends.includes(user.id))
+            return { success: false, error: 'Already friends' }
+        else if (!user.friendsRequests.includes(toAccept.id))
+            return { success: false, error: 'No friend request found' }
+        else {
+            await this.prismaService.user.update({
+                where: { name: username },
+                data: {
+                    friends: { push: toAccept.id },
+                    friendsRequests: { set: user.friendsRequests.filter(id => id != toAccept.id) }
+                }
+            })
+            await this.prismaService.user.update({
+                where: { name: toAcceptUsername },
+                data: { friends: { push: user.id } }
+            })
+            return { success: true }
+        }
+    }
+
+    async declineFriend(username: string, toDeclineUsername: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        const toDecline = await this.prismaService.user.findUnique({ where: { name: toDeclineUsername } })
+        if (toDecline == null)
+            return { success: false, error: 'Friend not found' }
+        if (user.friends.includes(toDecline.id) || toDecline.friends.includes(user.id))
+            return { success: false, error: 'Already friends' }
+        else if (!user.friendsRequests.includes(toDecline.id))
+            return { success: false, error: 'No friend request found' }
+        else {
+            await this.prismaService.user.update({
+                where: { name: username },
+                data: { friendsRequests: { set: user.friendsRequests.filter(id => id != toDecline.id) } }
+            })
+            return { success: true }
+        }
+    }
+
+    async deleteFriend(username: string, toDeleteUsername: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null)
+            return { success: false, error: 'User not found' }
+        const toDelete = await this.prismaService.user.findUnique({ where: { name: toDeleteUsername } })
+        if (toDelete == null)
+            return { success: false, error: 'Friend not found' }
+        if (!user.friends.includes(toDelete.id) || !toDelete.friends.includes(user.id))
+            return { success: false, error: 'Not friends' }
+        else {
+            await this.prismaService.user.update({
+                where: { name: username },
+                data: { friends: { set: user.friends.filter(id => id != toDelete.id) } }
+            })
+            await this.prismaService.user.update({
+                where: { name: toDeleteUsername },
+                data: { friends: { set: toDelete.friends.filter(id => id != user.id) } }
+            })
+            return { success: true }
+        }
+    }
+
+    // User related functions
+
+    async getUserInfos(username: string) {
+        const user = await this.prismaService.user.findUnique({ where: { name: username } })
+        if (user == null) {
+            return { success: false, error: 'User not found' }
+        }
+        //todo envoyer un historique?
+        return {
+            success: true, user: {
+                username: user.name,
+                image: user.image,
+                balance: user.balance,
+                statusMessage: user.statusMessage,
+            }
+        }
+    }
 
     async getOnlineUsers() {
         const users = await this.prismaService.user.findMany({

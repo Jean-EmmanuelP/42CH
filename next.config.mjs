ec2b41ddb04a771d904f563e6836898fb1b9be8c
Guillaume Ouint <g.ouint@otux.fr>
2023-08-05 04:45:39 +0200

    ignore lint

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,11 @@ const config = {
   images: {
     domains: ['lh3.googleusercontent.com', 'cdn.intra.42.fr'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;

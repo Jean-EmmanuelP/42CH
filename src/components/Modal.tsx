9eae2465c6f09dff6ee339e5efd9a3dcc595c07c
Jean-EmmanuelP <perramant.work@gmail.com>
2023-08-06 14:47:31 +0200

    added the echap for events

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,62 +1,76 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 interface ModalProperties {
-    isVisible: boolean,
-    onClose: () => void,
-    children: React.ReactNode;
-    width: string;
+  isVisible: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  width: string;
 }
 
 const Modal = ({ isVisible, onClose, children, width }: ModalProperties) => {
-    const [show, setShow] = useState(false);
-    const [transitioning, setTransitioning] = useState(false);
-  
-    useEffect(() => {
-      if (isVisible) {
-        setShow(true);
-        setTimeout(() => setTransitioning(true), 10); // Petite pause pour permettre le rendu initial
-      } else {
-        setTransitioning(false);
-        setTimeout(() => setShow(false), 500); // Durée de la transition
-      }
-    }, [isVisible]);
-  
-    const handleClose = (e: any) => {
-      if (e.target.id === 'wrapper') {
-        onClose();
-      }
-    };
-  
-    if (!show) return null;
-    return (
-      <div
-        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-50 transition-opacity duration-500"
-        id="wrapper"
-        onClick={handleClose}
-        style={{ opacity: isVisible ? 1 : 0 }}
-      >
-        <div
-          className={`${width} flex flex-col transition-transform duration-500`}
-          style={{ transform: transitioning ? 'translateY(0)' : 'translateY(-100%)' }}
-        >
-          <button className="text-white text-xl place-self-end" onClick={() => onClose()}>
-            X
-          </button>
-          <div className="bg-white p-2 rounded">{children}</div>
-        </div>
-      </div>
-    );
+  const [show, setShow] = useState(false);
+  const [transitioning, setTransitioning] = useState(false);
+
+  useEffect(() => {
+    if (isVisible) {
+      setShow(true);
+      setTimeout(() => setTransitioning(true), 10); // Petite pause pour permettre le rendu initial
+    } else {
+      setTransitioning(false);
+      setTimeout(() => setShow(false), 500); // Durée de la transition
+    }
+  }, [isVisible]);
+
+  const handleClose = (e: any) => {
+    if (e.target.id === "wrapper") {
+      onClose();
+    }
   };
-  
-  export default Modal;
-  
 
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            onClose()
+        }
+    };
 
-  
-  
-  
-  
-  
-  
+    if (isVisible) {
+        window.addEventListener('keydown', handleEscape);
+    } else {
+        window.removeEventListener('keydown', handleEscape);
+    }
+
+    return () => {
+        window.removeEventListener('keydown', handleEscape);
+    };
+  }, [isVisible, onClose])
+
+  if (!show) return null;
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm transition-opacity duration-500"
+      id="wrapper"
+      onClick={handleClose}
+      style={{ opacity: isVisible ? 1 : 0 }}
+    >
+      <div
+        className={`${width} flex flex-col transition-transform duration-500`}
+        style={{
+          transform: transitioning ? "translateY(0)" : "translateY(-100%)",
+        }}
+      >
+        <button
+          className="place-self-end text-xl text-white"
+          onClick={() => onClose()}
+        >
+          X
+        </button>
+        <div className="rounded bg-white p-2">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

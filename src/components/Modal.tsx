d82f92dd8eb973cf8b826a7dca8a1fde7fba89fe
Jean-EmmanuelP <perramant.work@gmail.com>
2023-08-06 11:57:32 +0200

    transition des evenements en cours, jai demande a gpt de me gere ca maintenant je vais check pour personnalise la transition et comprendre comment cela fonctionne

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 
 interface ModalProperties {
     isVisible: boolean,
@@ -7,21 +9,54 @@ interface ModalProperties {
     width: string;
 }
 
-const Modal = ({isVisible, onClose, children, width} : ModalProperties) => {
-    if (!isVisible) return null;
-    
-    const handleClose = (e:any) => {
-        if (e.target.id === 'wrapper' ) onClose();
-    }
-    console.log(`my width is`, width);
+const Modal = ({ isVisible, onClose, children, width }: ModalProperties) => {
+    const [show, setShow] = useState(false);
+    const [transitioning, setTransitioning] = useState(false);
+  
+    useEffect(() => {
+      if (isVisible) {
+        setShow(true);
+        setTimeout(() => setTransitioning(true), 10); // Petite pause pour permettre le rendu initial
+      } else {
+        setTransitioning(false);
+        setTimeout(() => setShow(false), 500); // Durée de la transition
+      }
+    }, [isVisible]);
+  
+    const handleClose = (e: any) => {
+      if (e.target.id === 'wrapper') {
+        onClose();
+      }
+    };
+  
+    if (!show) return null;
     return (
-        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-50' id="wrapper" onClick={handleClose}>
-            <div className={`${width} flex flex-col`}>
-                <button className='text-white text-xl place-self-end' onClick={() => onClose()}>X</button>
-                <div className='bg-white p-2 rounded'>{children}</div>
-            </div>
+      <div
+        className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-50 transition-opacity duration-500"
+        id="wrapper"
+        onClick={handleClose}
+        style={{ opacity: isVisible ? 1 : 0 }}
+      >
+        <div
+          className={`${width} flex flex-col transition-transform duration-500`}
+          style={{ transform: transitioning ? 'translateY(0)' : 'translateY(-100%)' }}
+        >
+          <button className="text-white text-xl place-self-end" onClick={() => onClose()}>
+            X
+          </button>
+          <div className="bg-white p-2 rounded">{children}</div>
         </div>
-    ) 
-}
+      </div>
+    );
+  };
+  
+  export default Modal;
+  
 
-export default Modal;
+
+  
+  
+  
+  
+  
+  

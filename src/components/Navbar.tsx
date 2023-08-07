f322b3497b30ca56f9e44c1349d983db7e05a23d
Jean-EmmanuelP <perramant.work@gmail.com>
2023-08-07 13:26:50 +0200

    navbar clickable image to modal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import getConfig from 'next/config';
 import Image from "next/image";
+import Modal from "./Modal";
+import EventProfileModal from "./EventProfileModal";
 const { publicRuntimeConfig } = getConfig();
 
 function extractUsername(email: string) {
@@ -16,6 +18,8 @@ function extractUsername(email: string) {
 export default function Navbar() {
   const [infoUser, setInfoUser] = useState<any>({})
   const [UserisHere, setUserIsHere] = useState(0);
+  const [showUserModal, setShowUserModal] = useState<boolean>(false);
+  const [eventToSend, setEventToSend] = useState<{ image: string, username: string, balance: number, statusMessage: string, classment: string }>({ image: "", username: "", balance: 0, statusMessage: "", classment: "" });
   
   useEffect(() => {
     userInfo()
@@ -62,7 +66,7 @@ export default function Navbar() {
         /> */}
       </div>
 
-      <a className= "mr-10 flex items-center space-x-3 p-4 hover:text-blue-700">
+      <button className= "mr-10 flex items-center space-x-3 p-4 hover:text-blue-700" onClick={() => { setShowUserModal(true), setEventToSend(infoUser) }}>
         {UserisHere !== 0 && (<Image
           src={infoUser.image}
           width={50}
@@ -71,7 +75,10 @@ export default function Navbar() {
           className="h-12 w-12 rounded-full shadow-md"
         />)}
         
-      </a>
+      </button>
+      <Modal isVisible={showUserModal} onClose={() => setShowUserModal(false)} width="w-[500px]">
+        <EventProfileModal userProfile={eventToSend} />
+      </Modal>
     </div>
   );
 }

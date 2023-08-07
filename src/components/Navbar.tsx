48364ad49e76729ee992e10e74ea13590738459b
Perramant <perramant.work@gmail.com>
2023-08-07 23:53:15 +0200

    done the logout button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import getConfig from 'next/config';
+import getConfig from "next/config";
 import Image from "next/image";
 import Modal from "./Modal";
 import EventProfileModal from "./EventProfileModal";
@@ -16,33 +16,37 @@ function extractUsername(email: string) {
 }
 
 export default function Navbar() {
-  const [infoUser, setInfoUser] = useState<any>({})
+  const [infoUser, setInfoUser] = useState<any>({});
   const [UserisHere, setUserIsHere] = useState(0);
   const [showUserModal, setShowUserModal] = useState<boolean>(false);
-  const [eventToSend, setEventToSend] = useState<{ image: string, username: string, balance: number, statusMessage: string, classment: string }>({ image: "", username: "", balance: 0, statusMessage: "", classment: "" });
-  
-  useEffect(() => {
-    userInfo()
-  }, [])
+  const [eventToSend, setEventToSend] = useState<{
+    image: string;
+    username: string;
+    balance: number;
+    statusMessage: string;
+    classment: string;
+  }>({ image: "", username: "", balance: 0, statusMessage: "", classment: "" });
 
- 
   useEffect(() => {
-    console.log(`User is here`, UserisHere)
-  }, [UserisHere])
+    userInfo();
+  }, []);
+
+  useEffect(() => {
+    console.log(`User is here`, UserisHere);
+  }, [UserisHere]);
 
   async function userInfo() {
     const username = sessionStorage.getItem("username");
-    if (username)
-    {
+    if (username) {
       setUserIsHere(1);
     }
     const request = await axios.post(
-      process.env.NEXT_PUBLIC_API_URL+"/user/get_user_infos",
+      process.env.NEXT_PUBLIC_API_URL + "/user/get_user_infos",
       JSON.stringify({ username: username }),
       { headers: { "Content-Type": "application/json" } }
     );
     if (request.data.success) {
-      console.log(request.data)
+      console.log(request.data);
       setInfoUser(request.data.user);
     }
   }
@@ -55,28 +59,58 @@ export default function Navbar() {
         <p className="pl-12 text-xs">Become the best challenger of 42</p>
       </div>
 
-      <div className="flex items-center h-full gap-10">
+      <div className="flex h-full items-center gap-10">
         <Link href="/classement" className="button mr-7 p-5">
           Classement
         </Link>
-        {/* <input 
+        <input
           type="text"
           placeholder="Rechercher un challenger"
-          className="shadow-lg rounded-lg h-[40%] px-2 py-4 placeholder:text-[12px] placeholder:text-center"
-        /> */}
+          className="h-[40%] rounded-lg px-2 py-4 shadow-lg placeholder:text-center placeholder:text-[12px]"
+        />
       </div>
 
-      <button className= "mr-10 flex items-center space-x-3 p-4 hover:text-blue-700" onClick={() => { setShowUserModal(true), setEventToSend(infoUser) }}>
-        {UserisHere !== 0 && (<Image
-          src={infoUser.image}
-          width={50}
-          height={50}
-          alt="Profile"
-          className="h-12 w-12 rounded-full shadow-md"
-        />)}
-        
-      </button>
-      <Modal isVisible={showUserModal} onClose={() => setShowUserModal(false)} width="w-[500px]">
+      <div>
+        <div className="flex">
+          <button
+            onClick={() => {
+              if (
+                confirm(
+                  "Are you sure ?"
+                )
+              ) {
+                sessionStorage.removeItem("username");
+                sessionStorage.removeItem("accessToken");
+              } else {
+              }
+              window.location.reload();
+            }}
+          >
+            Logout
+          </button>
+          <button
+            className="mr-10 flex items-center space-x-3 p-4 hover:text-blue-700"
+            onClick={() => {
+              setShowUserModal(true), setEventToSend(infoUser);
+            }}
+          >
+            {UserisHere !== 0 && (
+              <Image
+                src={infoUser.image}
+                width={50}
+                height={50}
+                alt="Profile"
+                className="h-12 w-12 rounded-full shadow-md"
+              />
+            )}
+          </button>
+        </div>
+      </div>
+      <Modal
+        isVisible={showUserModal}
+        onClose={() => setShowUserModal(false)}
+        width="w-[500px]"
+      >
         <EventProfileModal userProfile={eventToSend} />
       </Modal>
     </div>

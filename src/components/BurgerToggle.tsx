dd23a06c2318812c1081b3eb7e12a7b6ceb55ec8
achatela <achatela@student.42.fr>
2023-08-09 19:09:12 +0200

    redirection burger classement

--- src/components/BurgerToggle.tsx
+++ src/components/BurgerToggle.tsx
@@ -6,13 +6,13 @@ interface BurgerToggleProps {
     resetState: () => void;
 }
 
-export default function BurgerToggle({resetState, isOpen = false, onDefiClick, onClose, onSocialClick }:BurgerToggleProps) {
+export default function BurgerToggle({ resetState, isOpen = false, onDefiClick, onClose, onSocialClick }: BurgerToggleProps) {
     return (
         <div className={`absolute inset-0 top-[9%] bottom-[18%] bg-[#272A30] z-50`}>
             <div className="flex flex-col w-full h-full text-white items-center justify-center gap-6 text-[18px]">
-                <p onClick={ () => {onClose();resetState();onDefiClick();}}>Defi</p>
-                <p onClick={() => {onClose(); resetState();onSocialClick();}}>Social</p>
-                <p>Classement</p>
+                <p onClick={() => { onClose(); resetState(); onDefiClick(); }}>Defi</p>
+                <p onClick={() => { onClose(); resetState(); onSocialClick(); }}>Social</p>
+                <p onClick={() => { window.location.href = "/classement" }}>Classement</p>
             </div>
         </div>
     )

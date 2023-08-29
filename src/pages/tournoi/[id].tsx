3a1035c7a94c279f100920f7c40fe373406e9b36
achatela <achatela@student.42.fr>
2023-08-29 19:30:16 +0200

    .

--- src/pages/tournoi/[id].tsx
+++ src/pages/tournoi/[id].tsx
@@ -9,13 +9,21 @@ export default function TornamentTreePage() {
     const searchParams = useSearchParams();
     const tournoiName = searchParams.get('id');
     const [tourneyData, setTourneyData] = useState<any[]>([
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 0, column: 0 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 1, column: 0 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 2, column: 0 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 3, column: 0 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 0, column: 1 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 1, column: 1 },
-        { firstTeam: "a & b", secondTeam: "c & e", winner: "", rowPosition: 0, column: 2 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
+        { firstTeam: "none", secondTeam: "none", winner: "", rowPosition: 0, column: 0 },
     ]);
     // const [tourneyData, setTourneyData] = useState<any[]>([]);
     const [participants, setParticipants] = useState<number>(0);
@@ -32,15 +40,23 @@ export default function TornamentTreePage() {
         if (tournoiName) {
             const fetchData = async () => {
                 const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + `/tourney/get_tourney`, JSON.stringify({ tourneyTitle: tournoiName }), { headers: { 'Content-Type': 'application/json' } });
-                if (response.data.success == true)
-                    setTourneyData(response.data.tourney);
+                if (response.data.success == true) {
+                    setTourneyData((prev) => {
+                        const newTourneyData = [...prev];
+                        for (let i = 0; i < response.data.tourney.length; i++) {
+                            newTourneyData[i] = response.data.tourney[i];
+                        }
+                        return newTourneyData;
+                    });
+                    console.log("tourney", response.data.tourney)
+                }
                 else {
                     console.log(response.data.error);
                 }
             };
-            // fetchData();
+            fetchData();
         }
-    }, [tournoiName])
+    }, [])
 
     function nextTerm(a1: any, a_last: any, n: any) {
         if (n == 1)
@@ -48,6 +64,17 @@ export default function TornamentTreePage() {
         else {
             return a_last + Math.pow(2, n - 3) * a1;
         }
+    }
+
+    async function setRoundsNumber(num: number) {
+        if (num <= 4)
+            setRounds(2);
+        else if (num <= 8)
+            setRounds(3);
+        else if (num <= 16)
+            setRounds(4);
+        else if (num <= 32)
+            setRounds(5);
     }
 
     useEffect(() => {
@@ -58,17 +85,13 @@ export default function TornamentTreePage() {
             return acc;
         }, 0);
         setParticipants(count * 2);
-        setRounds(Math.log2(participants))
+        setRoundsNumber(count * 2)
+        setRoundsNumber(count / 2)
 
         let a1 = (100 / count);
         let a_last = 0;
         let marginTopArray: number[] = [];
 
-        // for (let i = 1; i <= count; i++) {
-        //     a_last = nextTerm(a1, a_last, i);
-        //     console.log(a_last)
-        //     marginTopArray.push(a_last);
-        // }
     }, []);
     // }, [tourneyData])
 
@@ -87,48 +110,136 @@ export default function TornamentTreePage() {
         console.log(marginTopArray),
 
         <div className='bg-white rounded-md shadow-md h-full w-full'>
-            {/* <div className='h-[3%] w-full'></div> */}
-            {/* <div className='h-[97%] w-full border border-red-500'> */}
-            {/* <p className='text-center text-[26px] w-full h-[5%]'>{tournoiName}</p> */}
             <div className={`w-full h-full border border-green-500 flex items-center`}>
+                {rounds === 2 ? (
+                    <>
+                        <div className='w-full h-full flex-row'>
+                            <div className='h-[50%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[0].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[0].secondTeam}</p>
+                            </div>
+                            <div className='h-[50%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[1].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[1].secondTeam}</p>
+                            </div>
+                        </div>
+                        <div className={`w-full h-[50%]`}>
+                            <div className='h-full w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[2].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[2].secondTeam}</p>
+                            </div>
+                        </div>
+                    </>) : null}
                 {rounds === 3 ? (
                     <>
                         <div className='w-full h-full flex-row'>
                             <div className='h-[25%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
-                            </div>
-
-                            <div className='h-[25%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[0].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[0].secondTeam}</p>
                             </div>
                             <div className='h-[25%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[1].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[1].secondTeam}</p>
                             </div>
                             <div className='h-[25%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[2].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[2].secondTeam}</p>
+                            </div>
+                            <div className='h-[25%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[3].firstTeam}ou</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[3].secondTeam}</p>
                             </div>
                         </div>
                         <div className={`w-full h-[75%]`}>
                             <div className='h-[33.33%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[4].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[4].secondTeam}</p>
                             </div>
                             <div className='h-[33.33%] w-full mt-[50%] flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[5].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[5].secondTeam}</p>
                             </div>
                         </div>
                         <div className={`w-full h-[25%]`}>
                             <div className='h-[98%] w-full flex flex-col justify-center items-center'>
-                                <p className='w-[65%] bg border border-red-500'>coucou</p>
-                                <p className='w-[65%] bg border border-red-500 mt-4'>coucou</p>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[6].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[6].secondTeam}</p>
                             </div>
                         </div>
                     </>) : null}
+                {rounds === 4 ? (
+                    <>
+                        <div className='w-full h-full flex-row'>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[0].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[0].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[1].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[1].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[2].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[2].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[3].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[3].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[4].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[4].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[5].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[5].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[6].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[6].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[7].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[7].secondTeam}</p>
+                            </div>
+                        </div>
+                        <div className={`w-full h-[87.5%] flex-row`}>
+                            <div className='h-[12.5%] w-full flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[8].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[8].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex mt-[41%] flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[9].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[9].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex mt-[41%] flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[10].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[10].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full flex mt-[41%] flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[11].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[11].secondTeam}</p>
+                            </div>
+                        </div>
+                        <div className={`w-full h-[75%]`}>
+
+                            <div className='h-[12.5%] w-full mt-[17.5%] flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[12].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[12].secondTeam}</p>
+                            </div>
+                            <div className='h-[12.5%] w-full mt-[122.5%] flex flex-col justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[13].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[13].secondTeam}</p>
+                            </div>
+                        </div>
+                        <div className={`w-full h-full`}>
+                            <div className='h-[12.5%] w-full flex flex-col mt-[122.5%] justify-center items-center'>
+                                <p className='w-[65%] bg border border-red-500'>{tourneyData[14].firstTeam}</p>
+                                <p className='w-[65%] bg border border-red-500 mt-4'>{tourneyData[14].secondTeam}</p>
+                            </div>
+                        </div>
+                    </>
+                ) : null}
             </div>
         </div>
         // </div>
